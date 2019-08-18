1c969bc9444ad09326c8a6e6d294f005310485c1
johannes <johannesboog@yahoo.de>
2019-08-18 14:58:10 +0200

    [ogs5_mmp] function to add mmp bloc implemented

---
+++ ogs5/input_mmp/input_add_mmp_bloc.R
@@ -0,0 +1,140 @@
+# function to add mmp_component to ogs5-obj
+# input: ogs5-obj
+# output: updated ogs5-obj
+
+input_add_mmp_bloc <- 
+   
+   function(
+      x = list(),
+      CAPILLARY_PRESSURE = NULL,
+      CHANNEL = FALSE,
+      COMPOUND_DEPENDENT_DT = NULL,
+      CONDUCTIVITY_MODEL = NULL,
+      CONVERSION_FACTOR = NULL,
+      DATA = NULL,
+      DIFFUSION = NULL,
+      DIS_TYPE = NULL,
+      ELEMENT_VOLUME_MULTIPLYER = NULL,
+      EVAPORATION = NULL,
+      FLOWLINEARITY = NULL,
+      GEOMETRY_AREA = character(NULL),
+      GEOMETRY_DIMENSION = character(NULL),
+      GEOMETRY_INCLINATION = NULL,
+      GEO_TYPE = NULL,
+      HEAT_DISPERSION = NULL,
+      HEAT_TRANSFER = NULL,
+      INTERPHASE_FRICTION = NULL,
+      MASS_DISPERSION = NULL,
+      MMP_TYPE = NULL,
+      MSH_TYPE = NULL,
+      NAME = character(NULL),
+      ORGANIC_CARBON = NULL,
+      PARTICLE_DIAMETER = NULL,
+      PCS_TYPE = NULL,
+      PERMEABILITY_DISTRIBUTION = NULL,
+      PERMEABILITY_FUNCTION_DEFORMATION = NULL,
+      PERMEABILITY_FUNCTION_EFFSTRESS = NULL,
+      PERMEABILITY_FUNCTION_POROSITY = NULL,
+      PERMEABILITY_FUNCTION_PRESSURE = NULL,
+      PERMEABILITY_FUNCTION_STRAIN = NULL,
+      PERMEABILITY_FUNCTION_STRESS = NULL,
+      PERMEABILITY_FUNCTION_VELOCITY = NULL,
+      PERMEABILITY_SATURATION = NULL,
+      PERMEABILITY_TENSOR  = NULL,
+      POROSITY = NULL,
+      POROSITY_DISTRIBUTION = NULL,
+      RILL = NULL,
+      SPECIFIC_STORAGE = NULL,
+      STORAGE = NULL,
+      STORAGE_FUNCTION_EFFSTRESS = NULL,
+      SURFACE_FRICTION = NULL,
+      TORTUOSITY = NULL,
+      TRANSFER_COEFFICIENT = NULL,
+      UNCONFINED = NULL,
+      VOL_BIO = NULL,
+      VOL_MAT = NULL,
+      WIDTH  = NULL
+      
+   ){
+      
+      mmp_name <- NAME
+      
+      # validate input
+      valid_ogs5(x)
+      
+      # look if ogs5-objinput$mmp exists and valid, otherwise create 
+      if (!("mmp" %in% names(x$input))) {
+         x$input$mmp <- create_ogs5_mmp() 
+      } else {
+         
+         valid_ogs5_mmp(x$input$mmp)
+         
+         if (mmp_name %in% names(x$input$mmp)) {
+            stop("mmp_name does already exist", call. = FALSE)
+         }
+         
+      }
+      
+      # create and add sublist to mmp-list
+      
+      x$input$mmp[[paste(mmp_name)]] <- list(
+         
+         "CAPILLARY_PRESSURE" = CAPILLARY_PRESSURE,
+         "CHANNEL" = CHANNEL,
+         "COMPOUND_DEPENDENT_DT" = COMPOUND_DEPENDENT_DT,
+         "CONDUCTIVITY_MODEL" = CONDUCTIVITY_MODEL,
+         "CONVERSION_FACTOR" = CONVERSION_FACTOR,
+         "DATA" = DATA,
+         "DIFFUSION" = DIFFUSION,
+         "DIS_TYPE" = DIS_TYPE,
+         "ELEMENT_VOLUME_MULTIPLYER" = ELEMENT_VOLUME_MULTIPLYER,
+         "EVAPORATION" = EVAPORATION,
+         "FLOWLINEARITY" = FLOWLINEARITY,
+         "GEOMETRY_AREA" = GEOMETRY_AREA,
+         "GEOMETRY_DIMENSION" = GEOMETRY_DIMENSION,
+         "GEOMETRY_INCLINATION" = GEOMETRY_INCLINATION,
+         "GEO_TYPE" = GEO_TYPE,
+         "HEAT_DISPERSION" = HEAT_DISPERSION,
+         "HEAT_TRANSFER" = HEAT_TRANSFER,
+         "INTERPHASE_FRICTION" = INTERPHASE_FRICTION,
+         "MASS_DISPERSION" = MASS_DISPERSION,
+         "MMP_TYPE" = MMP_TYPE,
+         "MSH_TYPE" = MSH_TYPE,
+         "NAME" = NAME,
+         "ORGANIC_CARBON" = ORGANIC_CARBON,
+         "PARTICLE_DIAMETER" = PARTICLE_DIAMETER,
+         "PCS_TYPE" = PCS_TYPE,
+         "PERMEABILITY_DISTRIBUTION" = PERMEABILITY_DISTRIBUTION,
+         "PERMEABILITY_FUNCTION_DEFORMATION" = PERMEABILITY_FUNCTION_DEFORMATION,
+         "PERMEABILITY_FUNCTION_EFFSTRESS" = PERMEABILITY_FUNCTION_EFFSTRESS,
+         "PERMEABILITY_FUNCTION_POROSITY" = PERMEABILITY_FUNCTION_POROSITY,
+         "PERMEABILITY_FUNCTION_PRESSURE" = PERMEABILITY_FUNCTION_PRESSURE,
+         "PERMEABILITY_FUNCTION_STRAIN" = PERMEABILITY_FUNCTION_STRAIN,
+         "PERMEABILITY_FUNCTION_STRESS" = PERMEABILITY_FUNCTION_STRESS,
+         "PERMEABILITY_FUNCTION_VELOCITY" = PERMEABILITY_FUNCTION_VELOCITY,
+         "PERMEABILITY_SATURATION" = PERMEABILITY_SATURATION,
+         "PERMEABILITY_TENSOR"  = PERMEABILITY_TENSOR,
+         "POROSITY" = POROSITY,
+         "POROSITY_DISTRIBUTION" = POROSITY_DISTRIBUTION,
+         "RILL" = RILL,
+         "SPECIFIC_STORAGE" = SPECIFIC_STORAGE,
+         "STORAGE" = STORAGE,
+         "STORAGE_FUNCTION_EFFSTRESS" = STORAGE_FUNCTION_EFFSTRESS,
+         "SURFACE_FRICTION" = SURFACE_FRICTION,
+         "TORTUOSITY" = TORTUOSITY,
+         "TRANSFER_COEFFICIENT" = TRANSFER_COEFFICIENT,
+         "UNCONFINED" = UNCONFINED,
+         "VOL_BIO" = VOL_BIO,
+         "VOL_MAT" = VOL_MAT,
+         "WIDTH"  = WIDTH
+         
+      ) %>% 
+         purrr::discard(is.null) %>% 
+         purrr::discard(isFALSE) %>% 
+         structure(class = "ogs5_mmp_bloc")
+      
+      valid_ogs5_mmp_bloc(x$input$mmp[[paste(mmp_name)]])
+      
+      return(x)
+      
+   }
