c44e033ca78819281fc0767bcd71c9de6b8aa7b3
joboog <johannesboog@yahoo.de>
2019-09-02 16:23:37 +0200

    [WriteInputfiles] prepare ogs5 blocs for input updated

--- ogs5/ogs5_write_inputfiles.R
+++ ogs5/ogs5_write_inputfiles.R
@@ -23,16 +23,11 @@
 #   }
 
 
-# function to print ogs5_pcs sublist ------------------------------------------
-ogs5_pcs_print_sublist <- 
-  
-  function(ogs5_sublist){
-    
-    # check ogs5_sublist
-    stopifnot(class(ogs5_sublist) == "ogs5_pcs")
-    
-    ogs5_mkey <- "PROCESS"
+# output simple sublist ---------------------------------------------
 
+ogs5_sublist_output <- 
+  function(ogs5_sublist = list(), ogs5_mkey = character()){
+    
     for (i in seq_len(ogs5_sublist %>% length())){
       ogs5_print_mkey_bloc(mkey_bloc = ogs5_sublist[[i]],
                            mkey = ogs5_mkey)
@@ -41,8 +36,43 @@ ogs5_pcs_print_sublist <-
     cat("STOP", "\n")
   }
 
-# function to print ogs5_bc sublist ------------------------------------------
-ogs5_pcs_print_sublist <- 
+# output an individual mkey bloc out of a ogs5_sublist -------------
+# input: mkey_bloc = main keywword bloc; mkey = main (#) key word
+# define as class specific method?
+ogs5_print_mkey_bloc <- 
+  
+  function(mkey_bloc = list(), mkey = character(NULL)){
+    
+    skey_str <- sapply(
+      names(mkey_bloc),
+      function(x) {
+        paste0("\n", "$", x, "\n ",
+               paste(mkey_bloc[[x]], collapse=" ")
+        )
+      }
+    )
+    
+    cat(paste0("#", mkey), skey_str, "\n")
+  }
+
+# output ogs5_pcs sublist ------------------------------------------
+ogs5_pcs_list_output <- 
+  
+  function(ogs5_sublist){
+    
+    # check ogs5_sublist
+    stopifnot(class(ogs5_sublist) == "ogs5_pcs")
+    
+    for (i in seq_len(ogs5_sublist %>% length())){
+      ogs5_print_mkey_bloc(mkey_bloc = ogs5_sublist[[i]],
+                           mkey = "PROCESS")
+      cat("\n")
+    }
+    cat("STOP", "\n")
+  }
+
+# output ogs5_bc sublist ------------------------------------------
+ogs5_bc_list_output <- 
   
   function(ogs5_sublist){
     
@@ -59,21 +89,110 @@ ogs5_pcs_print_sublist <-
     cat("STOP", "\n")
   }
   
-# function to print an individual mkey bloc out of a ogs5_sublist --------
-# input: mkey_bloc = main keywword bloc; mkey = main (#) key word
-# define as class specific method?
-ogs5_print_mkey_bloc <- 
+
+# output ogs5_cct sublist ------------------------------------------
+ogs5_cct_list_output <- 
   
-  function(mkey_bloc = list(), mkey = character(NULL)){
-  
-    skey_str <- sapply(
-                  names(mkey_bloc),
-                  function(x) {
-                    paste0("\n", "$", x, "\n ",
-                           paste(mkey_bloc[[x]], collapse=" ")
-                    )
-                  }
-                )
+  function(ogs5_sublist){
     
-    cat(paste0("#", mkey), skey_str, "\n")
+    # check ogs5_sublist
+    stopifnot(class(ogs5_sublist) == "ogs5_cct")
+    
+    ogs5_mkey <- ogs5_keywordlist$cct$mkey
+    
+    for (i in seq_len(ogs5_sublist %>% length())){
+      ogs5_print_mkey_bloc(mkey_bloc = ogs5_sublist[[i]],
+                           mkey = ogs5_mkey)
+      cat("\n")
+    }
+    cat("STOP", "\n")
   }
+
+# output ogs5_fct sublist ------------------------------------------
+ogs5_fct_list_output <- 
+  
+  function(ogs5_sublist){
+    
+    # check ogs5_sublist
+    stopifnot(class(ogs5_sublist) == "ogs5_fct")
+    
+    for (i in seq_len(ogs5_sublist %>% length())){
+      ogs5_print_fct_bloc(ogs5_fct_bloc = ogs5_sublist[[i]],
+                          mkey = "FUNCTION")
+      cat("\n")
+    }
+    cat("STOP", "\n")
+  }
+
+
+# output ogs5_fct bloc ----------------------------------------------------
+ogs5_fct_bloc_output <- 
+  
+  function(ogs5_fct_bloc){
+    
+    # check ogs5_sublist
+    stopifnot(class(ogs5_sublist) == "ogs5_fct_bloc")
+    
+    nn <- names(ogs5_fct_bloc)
+    
+    for (i in seq_len(length(ogs5_fct_bloc))){
+      
+      if (nn == "data_type") cat(ogs5_fct_bloc[[i]], "\n")
+      
+      if (nn == "data_values") 
+      ogs5_print_fct_bloc(fct_bloc = ogs5_sublist[[i]],
+                          mkey = "FUNCTION")
+      cat("\n")
+    }
+    cat("STOP", "\n")
+  }
+
+
+function(mkey_bloc = list(), mkey = character(NULL)){
+  
+  skey_str <- sapply(
+    names(mkey_bloc),
+    function(x) {
+      paste0("\n", "$", x, "\n ",
+             paste(mkey_bloc[[x]], collapse=" ")
+      )
+    }
+  )
+  
+  cat(paste0("#", mkey), skey_str, "\n")
+}
+# output ogs5_fct sublist ------------------------------------------
+
+# output ogs5_gem sublist ------------------------------------------
+
+# output ogs5_gli sublist ------------------------------------------
+
+# output ogs5_ic sublist ------------------------------------------
+
+# output ogs5_krc sublist ------------------------------------------
+
+# output ogs5_mcp sublist ------------------------------------------
+
+# output ogs5_mfp sublist ------------------------------------------
+
+# output ogs5_mmp sublist ------------------------------------------
+
+# output ogs5_msh sublist ------------------------------------------
+
+# output ogs5_msp sublist ------------------------------------------
+
+# output ogs5_num sublist ------------------------------------------
+
+# output ogs5_out sublist ------------------------------------------
+
+# output ogs5_pqc sublist ------------------------------------------
+
+# output ogs5_rei sublist ------------------------------------------
+
+# output ogs5_rfd sublist ------------------------------------------
+
+# output ogs5_st sublist ------------------------------------------
+
+# output ogs5_tim sublist ------------------------------------------
+
+
