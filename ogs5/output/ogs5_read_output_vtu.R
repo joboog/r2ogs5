084f700fe98823f0e329657d22d3c08b08a07c66
joboog <johannesboog@yahoo.de>
2020-02-19 10:23:37 +0100

    [ogs5-output] Added function to read *.vtu files series for specific process.

--- ogs5/output/ogs5_read_output_vtu.R
+++ ogs5/output/ogs5_read_output_vtu.R
@@ -9,13 +9,65 @@ dsa <- import("vtk.numpy_interface.dataset_adapter")
 
 # read multiple vtu files -------------------------------------------------
 
-# implement wrapper for ogs_read_vtu_file_point_data*
-# combine all vtu array data in tibble including mesh grid
-# implement for specific array and for all arrays
-# attach to ogs5-object
+ogs_read_vtu_files_point_data <- function(filepath = character(),
+                                          pcs_type = character(),
+                                          variable_name = NULL){
+  
+  # content:
+  # reads specific point data arrays from a series of vtu files in a specific
+  # directory, series is specified by the $PCS_TYPE given in the
+  # corresponding outbloc (ogs5_obj$input$out$...)
+  #
+  # pcs_type: name of the process defined in the ogs5_obj$input$out$.. bloc
+  # variable_name: name of the array in the *.vtu files (optional)
+  #
+  # returns a list
+  
+  
+  # check filepath
+  if (!(dir.exists(filepath))){
+    stop("'filepath' does not exist.", call = FALSE)
+  }
+  
+  # list *.vtu files
+  files <- list.files(
+              path=filepath,
+              pattern=".vtu",
+              full.names=TRUE
+            )[which(
+              stringr::str_detect(
+                string=list.files(
+                  path=filepath,
+                  pattern=".vtu",
+                  full.names=TRUE
+                ),
+                pattern=c(pcs_type)
+              )==TRUE
+            )]
+  
+  # loop over file list
+  l <- lapply(
+    files,
+    # read individual files
+    function(x) {
+      
+      if (is.null(variable_name)) {
+        ll <- ogs_read_vtu_file_point_data_all(filename = x)
+      } else {
+        ll <- ogs_read_vtu_file_point_data_array(filename = x,
+                                                array_name = variable_name)
+      }  
+      return(ll)
+    }
+  )
+  # return list for all files
+  names(l) <- files %>% str_remove(pattern = paste0(filepath, "/"))
+  return(l)
+}
 
 # read arrays from single *.vtu file --------------------------------------
 
+# read specific array from single file
 ogs_read_vtu_file_point_data_array <- 
     function(filename = character(),
              array_name = character()){
@@ -36,9 +88,12 @@ ogs_read_vtu_file_point_data_array <-
     src_data <- dsa$WrapDataObject(src$GetOutput())
     src_data_arr <- src_data$PointData[array_name]
     
-    return(as.vector(src_data_arr))
+    src_data_ll <- list(src_data_arr)
+    names(src_data_ll) <- array_name
+    return(src_data_ll)
 }
 
+# read all arrays from single file
 ogs_read_vtu_file_point_data_all <- 
     function(filename = character()){
         
