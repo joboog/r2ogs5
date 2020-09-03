57a19e40fe6efba39a7151dc4c5288249f333196
phit0 <philipp.schad@stud.uni-goettingen.de>
2020-09-04 00:28:06 +0200

    [Tests][Benchmarks] tests to create inputfiles from Engesgaard Benchmark
    reading/writing input files of Engesgaard benchmark. All except Phreeqc.dat and pds.pqc
    
    [Benchmarks][Tests] Include phreeqc.dat and pqc in tests

--- tests/testthat/setup-B-ogs5_read_inputfiles.R
+++ tests/testthat/setup-B-ogs5_read_inputfiles.R
@@ -1,13 +1,55 @@
+# read in ex1 input files -------------------------------------------------
 
 # create ogs5 object to compare to ex1 in subfolder ex1_read
 # tmp <- "examples/tmp/ex1"
 ex1_read <- create_ogs5(sim_name = "ex1_read", sim_id = 1L,
                         sim_path = paste0(tmp, "/ex1_read"))
 
-# debug(ogs5_get_ogs5_from_list)
 ex1_read <- input_add_blocs_from_file(ex1_read,
                                       filename = "all",
                                       file_dir = tmp)
 
 # debug(ogs5_list_output.ogs5_rfd)
 ogs5_write_inputfiles(ex1_read, type = "all")
+
+
+###########################################################################
+####                read in Engesgaard benchmark input files            ###
+###########################################################################
+
+
+# Engsesgaard/2Kin/slow_kin_pqc -------------------------------------------
+
+bm_dir <- "../../examples/benchmarks/Engesgaard/2Kin/slow_kin_pqc"
+#bm_dir <- "examples/benchmarks/Engesgaard/2Kin/slow_kin_pqc"
+
+# create new ogs5 object in temporary directory
+eg1_read <- create_ogs5(sim_name = "eg1_read", sim_id = 1L,
+                   sim_path = paste0(tmp, "/eg1_read"))
+
+# read in input files from benchmark folder
+eg1_read <- input_add_blocs_from_file(eg1_read,
+                                      filename = list("pds.msh",
+                                                      "pds.bc",
+                                                      "pds.gli",
+                                                      "pds.ic",
+                                                      "pds.mcp",
+                                                      "pds.mfp",
+                                                      "pds.mmp",
+                                                      "pds.msh",
+                                                      "pds.msp",
+                                                      "pds.num",
+                                                      "pds.out",
+                                                      "pds.pcs",
+                                                      "pds.rei",
+                                                      "pds.tim",
+                                                      "pds.st",
+                                                      "phreeqc.dat",
+                                                      "pds.pqc"),
+                                      file_dir = bm_dir)
+
+ogs5_write_inputfiles(eg1_read, type = "all")
+
+# Engsesgaard/2Kin/slow_kin_pqc_kcr -------------------------------------------
+
+# TODO
--- tests/testthat/test-ogs5_read_inputfiles.R
+++ tests/testthat/test-ogs5_read_inputfiles.R
@@ -1,15 +1,15 @@
 context("reading and writing of input files - EX1")
 
 # ex1_read object from setup-B
-input <- ex1_read$input
+ex1_input <- ex1_read$input
 
 test_that("respective classes are assigned", {
 
     #input <- ex1_read$input
     # bc classes
-    expect_silent(valid_ogs5_bc(input$bc))
+    expect_silent(valid_ogs5_bc(ex1_input$bc))
     expect_silent(valid_ogs5_bc_condition(
-        input$bc$BOUNDARY_CONDITION1
+        ex1_input$bc$BOUNDARY_CONDITION1
     ))
     # cct classes
     # MISSING
@@ -18,80 +18,80 @@ test_that("respective classes are assigned", {
     # gem clases
     # MISSING
     # gli classes
-    expect_silent(valid_ogs5_gli(input$gli))
-    expect_silent(valid_ogs5_gli_points(input$gli$POINTS1))
+    expect_silent(valid_ogs5_gli(ex1_input$gli))
+    expect_silent(valid_ogs5_gli_points(ex1_input$gli$POINTS1))
 
     # ic classes
-    expect_silent(valid_ogs5_ic(input$ic))
+    expect_silent(valid_ogs5_ic(ex1_input$ic))
     expect_silent(valid_ogs5_ic_condition(
-        input$ic$INITIAL_CONDITION1
+        ex1_input$ic$INITIAL_CONDITION1
     ))
     # krc classes
     # MISSING
     # mcp classes
-    expect_silent(valid_ogs5_mcp(input$mcp))
+    expect_silent(valid_ogs5_mcp(ex1_input$mcp))
     expect_silent(valid_ogs5_mcp_component(
-        input$mcp$COMPONENT_PROPERTIES1
+        ex1_input$mcp$COMPONENT_PROPERTIES1
     ))
     # mfp classes
-    expect_silent(valid_ogs5_mfp(input$mfp))
+    expect_silent(valid_ogs5_mfp(ex1_input$mfp))
     expect_silent(valid_ogs5_mfp_bloc(
-        input$mfp$FLUID_PROPERTIES1
+        ex1_input$mfp$FLUID_PROPERTIES1
     ))
     # mmp classes
-    expect_silent(valid_ogs5_mmp(input$mmp))
+    expect_silent(valid_ogs5_mmp(ex1_input$mmp))
     expect_silent(valid_ogs5_mmp_bloc(
-        input$mmp$MEDIUM_PROPERTIES1
+        ex1_input$mmp$MEDIUM_PROPERTIES1
     ))
     # msh classes
-    expect_silent(valid_ogs5_msh(input$msh))
-    expect_silent(valid_ogs5_msh_bloc(input$msh$FEM_MSH1))
+    expect_silent(valid_ogs5_msh(ex1_input$msh))
+    expect_silent(valid_ogs5_msh_bloc(ex1_input$msh$FEM_MSH1))
 
     # msp classes
-    expect_silent(valid_ogs5_msp(input$msp))
+    expect_silent(valid_ogs5_msp(ex1_input$msp))
     expect_silent(valid_ogs5_msp_bloc(
-        input$msp$SOLID_PROPERTIES1
+        ex1_input$msp$SOLID_PROPERTIES1
     ))
     # num classes
-    expect_silent(valid_ogs5_num(input$num))
+    expect_silent(valid_ogs5_num(ex1_input$num))
     expect_silent(valid_ogs5_num_bloc(
-        input$num$NUMERICS1
+        ex1_input$num$NUMERICS1
     ))
     # out classes
-    expect_silent(valid_ogs5_out(input$out))
+    expect_silent(valid_ogs5_out(ex1_input$out))
     expect_silent(valid_ogs5_out_bloc(
-        input$out$OUTPUT1
+        ex1_input$out$OUTPUT1
     ))
     # pcs classes
-    expect_silent(valid_ogs5_pcs(input$pcs))
-    expect_silent(valid_ogs5_pcs_process(input$pcs$PROCESS1))
+    expect_silent(valid_ogs5_pcs(ex1_input$pcs))
+    expect_silent(valid_ogs5_pcs_process(ex1_input$pcs$PROCESS1))
 
     # pqc classes
 
     # rei classes
 
     #rfd class
-    expect_silent(valid_ogs5_rfd(input$rfd))
+    expect_silent(valid_ogs5_rfd(ex1_input$rfd))
 
     # tim classes
-    expect_silent(valid_ogs5_tim(input$tim))
+    expect_silent(valid_ogs5_tim(ex1_input$tim))
     expect_silent(valid_ogs5_tim_bloc(
-        input$tim$TIME_STEPPING1
+        ex1_input$tim$TIME_STEPPING1
     ))
     # st classes
-    expect_silent(valid_ogs5_st(input$st))
+    expect_silent(valid_ogs5_st(ex1_input$st))
     expect_silent(valid_ogs5_st_condition(
-        input$st$SOURCE_TERM1
+        ex1_input$st$SOURCE_TERM1
     ))
     # mmp classes
-    expect_silent(valid_ogs5_mmp(input$mmp))
+    expect_silent(valid_ogs5_mmp(ex1_input$mmp))
     expect_silent(valid_ogs5_mmp_bloc(
-        input$mmp$MEDIUM_PROPERTIES1
+        ex1_input$mmp$MEDIUM_PROPERTIES1
     ))
 })
 
 # loop over input files and compare
-for (file_ext in names(input)) {
+for (file_ext in names(ex1_input)) {
 
     test_that(
         paste0("Reading and writing inputfile .",
@@ -136,6 +136,58 @@ test_that("read-in input files can be used to run ogs5", {
 
 })
 
+
+# Engesgaard read input tests -----------------------------------------------
+
+context("Read Engesgaard benchmakr input files")
+
+# loop over input files and compare
+for (file_ext in names(eg1_read$input)) {
+
+    test_that(
+        paste0("Reading and writing inputfile .",
+               file_ext,
+               " changes nothing"), {
+
+        # read-in and written vector (see setup-B)
+        read_vec <- scan(file = paste0(tmp, "/eg1_read/eg1_read." , file_ext),
+                         what = "character",
+                         blank.lines.skip = TRUE,
+                         sep = "\n",
+                         quiet = TRUE) %>%
+                    stringr::str_squish()
+        # extra remove empty lines
+        read_vec <- read_vec[which(read_vec != "")]
+        read_vec <- read_vec[which(read_vec != "#")] # remove '#' lines
+
+        if (file_ext == "dat") {
+            filepath <- paste0(bm_dir, "/phreeqc.dat")
+        } else {
+            filepath <- paste0(bm_dir, "/pds.", file_ext)
+        }
+        # original vector from ex1
+        orig_vec <- scan(file = filepath,
+                         what = "character",
+                         blank.lines.skip = TRUE,
+                         sep = "\n",
+                         quiet = TRUE) %>%
+                    stringr::str_squish()
+        orig_vec <- orig_vec[which(orig_vec != "")]
+        orig_vec <- orig_vec[which(orig_vec != "#")]
+
+        # remove elements after 'STOP'
+        if (length(orig_vec) != length(read_vec)) {
+            orig_vec <- orig_vec[
+                -c((which(orig_vec == "#STOP") + 1):length(orig_vec))
+            ]
+        }
+
+
+
+        # TEST
+        expect_equal(orig_vec, read_vec)
+               })
+}
 
 
 
