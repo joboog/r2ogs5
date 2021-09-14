7d2fbe41b722e05890dc36f7cdee76522eda03c1
phit0 <philipp.schad@stud.uni-goettingen.de>
2021-09-14 13:26:19 +0200

    [ci] remove packrat
    [ci] debug5
    
    [ci] without packrat
    
    [ci] with suggests

---
+++ R/ci-setup.R
@@ -0,0 +1,13 @@
+pkgs <- scan("./DESCRIPTION", what = "character", quiet = TRUE)
+
+
+pkgs <- stringr::str_remove(
+    na.omit(
+        pkgs[c((which(pkgs == "Imports:") + 1):
+                   (which(pkgs == "Depends:") - 1),
+               (which(pkgs == "Suggests:") + 1):
+                   (which(pkgs == "VignetteBuilder:") - 1))]),
+                            pattern = ",")
+
+
+install.packages(setdiff(pkgs, rownames(installed.packages())))
